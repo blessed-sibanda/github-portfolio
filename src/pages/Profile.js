0152022d5b6c7f853b727718cf175fe6e7bd18c9
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-05-06 03:44:34 +0200

    display github portfolio using components

---
+++ src/pages/Profile.js
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import Link from "../components/Link";
+import List from "../components/List";
+import "./Profile.css";
+
+export default function Profile({ userName }) {
+  const [loading, setLoading] = useState(false);
+  const [profile, setProfile] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const profile = await fetch(`https://api.github.com/users/${userName}`);
+      const result = await profile.json();
+
+      if (result) {
+        setProfile(result);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [userName]);
+
+  const items = [
+    {
+      field: "html_url",
+      value: <Link url={profile.html_url} title={profile.html_url} />,
+    },
+    {
+      field: "repos_url",
+      value: <Link url={profile.repos_url} title={profile.repos_url} />,
+    },
+    {
+      field: "name",
+      value: profile.name,
+    },
+    {
+      field: "company",
+      value: profile.company,
+    },
+    {
+      field: "location",
+      value: profile.location,
+    },
+    {
+      field: "email",
+      value: profile.email,
+    },
+    {
+      field: "bio",
+      value: profile.bio,
+    },
+  ];
+
+  return (
+    <div className="Profile-container">
+      <h2>About me</h2>
+      {loading ? (
+        <span>Loading...</span>
+      ) : (
+        <>
+          <img
+            src={profile.avatar_url}
+            alt={profile.name}
+            className="Profile-avatar"
+          />
+          <List items={items} />
+        </>
+      )}
+    </div>
+  );
+}
